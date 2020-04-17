b70580f30754b7060a609b53e2551c3477a52209
mmasaoy <mrcmasaoy@gmail.com>
2020-04-16 23:17:34 -1000

    Assignment1

---
+++ Masaoy_Marc_Assignment1/public/product_data.js
@@ -0,0 +1,33 @@
+[
+    {
+    "shirt": "Original",
+    "price": 15.00,
+    "image": "./images/shirtkpop.jpg"
+    },
+    {
+    "shirt": "BlackPink",
+    "price": 25.00,
+    "image": "./images/shirtblackpink.jpg"
+    },
+    {
+    "shirt": "BTS",
+    "price": 30.00,
+    "image": "./images/shirtBTS.jpg"
+    },
+    {
+    "shirt": "GOT7",
+    "price": 30.00,
+    "image": "./images/shirtgot7.jpg"
+    },
+    {
+    "shirt": "Itzy",
+    "price": 30.00,
+    "image": "./images/shirtitzy.jpg"
+    },
+    {
+    "shirt": "MonstaX",
+    "price": 35.00,
+    "image": "./images/shirtmonstax.jpg"
+    }
+    
+  ]
