d3d394c90f1931aef000c290d9c862643a23b4ed
mmasaoy <59866522+mmasaoy@users.noreply.github.com>
2020-05-15 10:37:23 -1000

    Add files via upload

---
+++ Public/Public/product_data.js
@@ -0,0 +1,50 @@
+//Assignment 1 by Shane Gimeenez//
+//codes are from lab 13 exercise 3//
+// Also had help from Classmate Kiara Furutani//
+//Images are own by me (Shane Gimenez)//
+
+
+
+products =
+[
+    {
+        // Product 1
+        "Facemask": "Classic",
+        "price": 10,
+        "image": "./images/1.jpeg"
+    },
+    {
+        // Product 2
+        "Facemask": "Aloha",
+        "price": 15,
+        "image": "./images/2.jpeg"
+    },
+    {
+        // Product 3
+        "Facemask": "Shy Gray",
+        "price": 15,
+        "image": "./images/3.jpeg"
+    },
+    {
+        // Product 4
+        "Facemask": "Travel Blue",
+        "price": 15,
+        "image": "./images/4.jpeg"
+    },
+    {
+        // Product 5
+        "Facemask": "Baby Feels",
+        "price": 15,
+        "image": "./images/5.jpeg"
+    },
+    {
+        // Product 6
+        "Facemask": "Dark Flower",
+        "price": 15,
+        "image": "./images/6.jpeg"
+    }
+];
+
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
